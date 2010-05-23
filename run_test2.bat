05c9597449665dea1743ccfd14abf818fb01edeb
peterwilliams97 <peter.williams.97@gmail.com>
2010-05-24 02:12:56 +1000

    Done for the night

--- run_test2.bat
+++ run_test2.bat
@@ -4,6 +4,7 @@ echo Please set the jade_root environment variable to the root of your jade inst
 goto Done
 
 :Start
+@echo on
 java -cp %jade_root%\lib\jade.jar;%jade_root%\lib\jadeTools.jar;%jade_root%\lib\iiop.jar;%jade_root%\lib\commons-codec\commons-codec-1.3.jar;ordering.jar jade.Boot -agents Buyer1:examples.ordering.BuyerAgent(order1.csv)
 
 :Done

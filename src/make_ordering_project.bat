d8b3e94d70f206c78f4e903dae907cd5fdfab7be
peterwilliams97 <peter.williams.97@gmail.com>
2010-05-23 20:41:54 +1000

    first cut at ordering project

---
+++ src/make_ordering_project.bat
@@ -0,0 +1,22 @@
+REM Clean out old versions
+del examples\ordering\*.class
+del ordering.jar
+del ordering.zip
+
+REM Make the agents and copy them to launch directory
+javac -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.; examples/ordering/BookSellerGui.java 
+javac -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.; examples/ordering/BuyerAgent.java
+javac -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.; examples/ordering/SupplierAgent.java
+
+REM Pack the agents classes in a jar file
+jar cf ordering.jar examples/ordering/*.class
+
+REM Copy the jar file into Jade directory
+copy ordering.jar ..
+
+REM Check that classes are visible in classpath and well-formed
+java -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.; examples.ordering.BuyerAgent
+java -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.; examples.ordering.SupplierAgent
+java -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.; examples.ordering.BookSellerGui
+
+copy ordering.jar ordering.zip

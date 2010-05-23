444e08c5fe93a491115a101e1e9ce633a3b3855f
peterwilliams97 <peter.williams.97@gmail.com>
2010-05-24 07:10:13 +1000

    Cleaning up examples

--- run_test5.bat
+++ run_test5.bat
@@ -1,1 +1,10 @@
-java -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;ordering.jar  jade.Boot -agents Seller1:examples.ordering.SupplierAgent(catalog1.csv);Buyer2:examples.ordering.BuyerAgent(order2.csv);Buyer3:examples.ordering.BuyerAgent(order2.csv);Buyer4:examples.ordering.BuyerAgent(order2.csv);Buyer5:examples.ordering.BuyerAgent(order2.csv);Buyer6:examples.ordering.BuyerAgent(order2.csv);
+@echo off
+if not "%jade_root%" == "" goto Start
+echo Please set the jade_root environment variable to the root of your jade installation
+goto Done
+
+:Start
+@echo on
+java -cp %jade_root%\lib\jade.jar;%jade_root%\lib\jadeTools.jar;%jade_root%\lib\iiop.jar;%jade_root%\lib\commons-codec\commons-codec-1.3.jar;ordering.jar  jade.Boot -agents Seller1:examples.ordering.SupplierAgent(catalog1.csv);Buyer2:examples.ordering.BuyerAgent(order2.csv);Buyer3:examples.ordering.BuyerAgent(order2.csv);Buyer4:examples.ordering.BuyerAgent(order2.csv);Buyer5:examples.ordering.BuyerAgent(order2.csv);Buyer6:examples.ordering.BuyerAgent(order2.csv);
+
+:Done

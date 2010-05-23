d6461324c9f5a1fb5fba7e1f4f46e9d76164cdd3
peterwilliams97 <peter.williams.97@gmail.com>
2010-05-24 02:12:42 +1000

    Done for the night

--- run_test4.bat
+++ run_test4.bat
@@ -1,1 +1,10 @@
-java -cp \java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;ordering.jar  jade.Boot -agents Buyer1:examples.ordering.BuyerAgent(order2.csv);Seller1:examples.ordering.SupplierAgent(catalog1.csv)
+@echo off
+if not "%jade_root%" == "" goto Start
+echo Please set the jade_root environment variable to the root of your jade installation
+goto Done
+
+:Start
+@echo on
+java -cp %jade_root%\lib\jade.jar;%jade_root%\lib\jadeTools.jar;%jade_root%\lib\iiop.jar;%jade_root%\lib\commons-codec\commons-codec-1.3.jar;ordering.jar  jade.Boot -agents Buyer1:examples.ordering.BuyerAgent(order2.csv);Seller1:examples.ordering.SupplierAgent(catalog1.csv)
+
+:Done

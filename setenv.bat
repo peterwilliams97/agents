b12a8d4f058a65b7dbfe850e053134d85073d0b5
peterwilliams97 <peter.williams.97@gmail.com>
2010-05-23 12:23:27 +1000

    Starting to comprehend Jade run-time

--- setenv.bat
+++ setenv.bat
@@ -1,1 +1,3 @@
-set CLASSPATH=%CLASSPATH%;\java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.
+set CLASSPATH=%CLASSPATH%;\java\jade\lib\jade.jar;\java\jade\lib\jadeTools.jar;\java\jade\lib\iiop.jar
+set CLASSPATH=%CLASSPATH%;\java\jade\lib\commons-codec\commons-codec-1.3.jar;.
+set CLASSPATH=%CLASSPATH%;BookBuyerAgent.jar;BookSellerAgent.jar;BookSellerGui.jar
